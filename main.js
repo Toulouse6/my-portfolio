cb4cd1a34c98cb7dd76ac00556f4e390e6aaf084
Touluse <tal.argamanbib@gmail.com>
2024-12-30 23:22:27 +0200

    stack-filter

--- main.js
+++ main.js
@@ -110,4 +110,22 @@ function resetFilter() {
     if (filteredSection) {
         filteredSection.remove();
     }
+
 }
+
+// Scroll nav links
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop - 70,
+                    behavior: 'smooth',
+                });
+            }
+        });
+    });
+});
+
